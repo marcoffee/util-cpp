24a942344971f1136ba0de0dadf29a27c99faf2e
Marco <speaktomarco@gmail.com>
2018-06-12 08:49:13 -0300

    Allocator class to profile memory

---
+++ allocator.cc
@@ -0,0 +1,20 @@
+#include "allocator.hh"
+
+namespace util::allocator {
+
+  static std::atomic_uintmax_t _peak = 0, _total = 0, _freed = 0;
+
+  void add_memory (uintmax_t const size) {
+    _total += size;
+    util::atomic_max(_peak, now());
+  }
+
+  void free_memory (uintmax_t const size) {
+    _freed += size;
+  }
+
+  uintmax_t peak (void) { return _peak.load(std::memory_order_relaxed); }
+  uintmax_t total (void) { return _total.load(std::memory_order_relaxed); }
+  uintmax_t freed (void) { return _freed.load(std::memory_order_relaxed); }
+  uintmax_t now (void) { return total() - freed(); }
+}
---
+++ allocator.hh
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <atomic>
+#include <memory>
+#include <iostream>
+#include "base.hh"
+
+namespace util::allocator {
+
+  void add_memory (uintmax_t amount);
+  void free_memory (uintmax_t amount);
+
+  uintmax_t peak (void);
+  uintmax_t total (void);
+  uintmax_t freed (void);
+  uintmax_t now (void);
+
+  template <typename T>
+  class allocator {
+  public:
+    using base = std::allocator<T>;
+
+  private:
+    base _alloc;
+
+  public:
+    using value_type = typename base::value_type;
+    using size_type = typename base::size_type;
+    using difference_type = typename base::difference_type;
+    using propagate_on_container_move_assignment = typename base::propagate_on_container_move_assignment;
+    using is_always_equal = typename base::is_always_equal;
+
+    template <typename... ARGS>
+    allocator (ARGS&&... args) : _alloc(std::forward<ARGS>(args)...) {}
+
+    template <typename... ARGS>
+    [[nodiscard]] T* allocate (std::size_t n, ARGS&&... args) {
+      add_memory(n * sizeof(T));
+      return this->_alloc.allocate(n, std::forward<ARGS>(args)...);
+    }
+
+    template <typename... ARGS>
+    void deallocate (T* p, std::size_t n, ARGS&&... args) {
+      free_memory(n * sizeof(T));
+      this->_alloc.deallocate(p, n, std::forward<ARGS>(args)...);
+    }
+
+    bool operator == (allocator const& ot) const {
+      return this->_alloc == ot._alloc;
+    }
+
+    bool operator != (allocator const& ot) const {
+      return this->_alloc != ot._alloc;
+    }
+
+  };
+
+};

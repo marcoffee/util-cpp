7bb46a8e9f5975a22fdf3696a79cf2c15abecac3
Marco <speaktomarco@gmail.com>
2018-07-14 01:14:11 -0300

    Partial nsga code

--- evolution/nsga/base.hh
+++ evolution/nsga/base.hh
@@ -5,7 +5,7 @@
 namespace util::evolution {
 
   __EVO_TMPL_HEAD
-  class nsga : __BASE_CLASS {
+  class nsga : public __BASE_CLASS {
 
   public:
     __EVO_USING_TYPES;
@@ -15,9 +15,11 @@ namespace util::evolution {
 
   protected:
     siz_t _popsize;
-    difference* _dif;
+    siz_t _children;
+    siz_t *_fro;
+    dis_t *_dis;
 
-    bool dominates (fit_t const& fa, fit_t const& fb, bool* a_d, bool* b_d) {
+    bool dominates (fit_t const& fa, fit_t const& fb, bool& a_d, bool& b_d) {
       bool a_dom = false;
       bool b_dom = false;
 
@@ -38,73 +40,108 @@ namespace util::evolution {
         }
       }
 
-      *a_d = a_dom;
-      *b_d = b_dom;
+      a_d = a_dom;
+      b_d = b_dom;
 
       return a_dom ^ b_dom;
     }
 
-    void nd_sorting (fit_t const* fit, siz_t* ranks, siz_t size) {
+    siz_t nd_sorting (
+      fit_t const* fit, siz_t* ranks, siz_t* ranked, siz_t* ends,
+      siz_t size, siz_t fill
+    ) {
+      if (fill == 0) {
+        return 0;
+      }
+
       siz_v* dominated = new siz_v[size]();
       siz_t* count = new siz_t[size]();
-      siz_t* sorted = new siz_t[size];
-      siz_t* queue = sorted;
+      siz_t* queue = ranked;
+      siz_t* start = ranked;
 
       for (siz_t i = 0; i < size; ++i) {
         bool i_d, j_d;
 
         for (siz_t j = i + 1; j < size; ++j) {
-          if (dominates(fit[i], fit[j], i_d, j_d)) {
+          if (this->dominates(fit[i], fit[j], i_d, j_d)) {
             if (i_d) {
               dominated[i].emplace_back(j);
+              // std::cout << i << " dominates " << j << std::endl;
               count[j]++;
             } else {
               dominated[j].emplace_back(i);
+              // std::cout << j << " dominates " << i << std::endl;
               count[i]++;
             }
           }
         }
 
         if (count[i] == 0) {
-          *(sorted++) = i;
           ranks[i] = 0;
+          *(ranked++) = i;
         }
       }
 
-      while (queue != sorted) {
-        siz_t chr = *(queue++);
+      siz_t fronts = 0;
+      siz_t found = 0;
+      bool stop = false;
 
-        for (siz_t dom : dominated[chr]) {
-          if (--count[dom] == 0) {
-            ranks[dom] = ranks[chr] + 1;
-            *(sorted++) = dom;
+      while (queue != ranked && !stop) {
+        siz_t const pos = *(queue++);
+
+        for (siz_t const dom : dominated[pos]) {
+          if (--count[dom] != 0) {
+            continue;
+          }
+
+          ranks[dom] = ranks[pos] + 1;
+
+          if (ranks[dom] != fronts) {
+            found += ranked - start;
+            ends[fronts++] = found;
+            start = ranked;
+            stop = found >= fill;
+          }
+
+          *(ranked++) = dom;
+
+          if (stop) {
+            break;
           }
         }
       }
 
-      delete[] sorted;
+      if (!stop) {
+        found += ranked - start;
+        ends[fronts++] = found;
+      }
+
       delete[] count;
       delete[] dominated;
+
+      return fronts;
     }
 
-    void cd_sorting (fit_t const* fit, double* distance, siz_t size) {
+    void cd_sorting (fit_t const* fit, dis_t* distance, siz_t* order, siz_t size) {
       siz_t const last = size - 1;
       siz_t* idx = new siz_t[size];
 
-      std::fill(distance, distance + size, 0.0);
+      for (siz_t i = 0; i < size; ++i) {
+        distance[order[i]] = 0.0;
+      }
 
-      for (uintmax_t i = 0; i < this->dimensions(); ++i) {
-        std::iota(idx, idx + size, 0);
+      for (siz_t i = 0; i < this->dimensions(); ++i) {
+        std::copy(order, order + size, idx);
         std::stable_sort(idx, idx + size, this->build_compare(fit, i));
 
         siz_t const id_a = idx[0], id_b = idx[last];
-        double const delta = 1.0 / this->difference(fit[id_a], fit[id_b], i);
+        dis_t const delta = this->subtract(fit[id_a], fit[id_b], i);
 
-        distance[id_a] = std::numeric_limits<double>::infinity();
-        distance[id_b] = std::numeric_limits<double>::infinity();
+        distance[id_a] = std::numeric_limits<dis_t>::infinity();
+        distance[id_b] = std::numeric_limits<dis_t>::infinity();
 
         for (siz_t j = 1; j < last; ++j) {
-          distance[idx[j]] += delta * this->difference(
+          distance[idx[j]] += delta * this->subtract(
             fit[idx[j - 1]], fit[idx[j + 1]], i
           );
         }
@@ -114,7 +151,76 @@ namespace util::evolution {
       delete[] idx;
     }
 
+    siz_t initialize (chr_t* chr, fit_t* fit, siz_t) override {
+      return this->evo_t::initialize(chr, fit, this->popsize());
+    }
 
+    siz_t evolve (chr_t* chr, fit_t* fit, siz_t) override {
+      return this->evo_t::evolve(chr, fit, this->children());
+    }
+
+    siz_t select (chr_t* chr, fit_t* fit, siz_t, siz_t all) override {
+      siz_t* ranked = new siz_t[all];
+      siz_t* ends = new siz_t[all];
+
+      siz_t const fronts = this->nd_sorting(fit, this->_fro, ranked, ends, all, this->popsize());
+      siz_t const found = ends[fronts - 1];
+      siz_t start = 0;
+
+      std::fill(this->_dis, this->_dis + found, -std::numeric_limits<dis_t>::infinity());
+
+      for (siz_t i = 0; i < fronts; ++i) {
+        this->cd_sorting(fit, this->_dis, ranked + start, ends[i] - start);
+        start = ends[i];
+      }
+
+      if (found > this->popsize()) {
+        siz_t const conti = ends[fronts - 2];
+        siz_t const rem = this->popsize() - conti;
+        std::function<bool(siz_t, siz_t)> const cmp{
+          [ dis = this->_dis ] (siz_t a, siz_t b) { return dis[b] > dis[a]; }
+        };
+
+        util::iterator::multipartition(cmp,
+          ranked + conti, ranked + found, rem, ranked + conti
+        );
+      }
+
+      util::iterator::multiorder(
+        ranked, ranked + this->popsize(), 0, chr, fit, this->_dis, this->_fro
+      );
+
+      delete[] ends;
+      delete[] ranked;
+      return this->popsize();
+    }
+
+  public:
+
+    nsga (siz_t popsize, siz_t children, siz_t dimensions, siz_t seed)
+      : evo_t(dimensions, popsize + children, seed),
+        _popsize(popsize), _children(children) {}
+
+    nsga (siz_t popsize, siz_t dimensions, siz_t seed)
+      : nsga(popsize, popsize, dimensions, seed) {}
+
+    evo_t* copy (void) const override { return new nsga(*this); }
+
+    siz_t const& popsize (void) const { return this->_popsize; }
+    siz_t const& children (void) const { return this->_children; }
+
+    siz_t test_nd_sorting (
+      fit_t const* fit, siz_t* ranks, siz_t* ranked, siz_t* ends,
+      siz_t size, siz_t fill
+    ) {
+      return this->nd_sorting(fit, ranks, ranked, ends, size, fill);
+    }
+
+    void test_cd_sorting (
+      fit_t const* fit, dis_t* distance, siz_t* order, siz_t size
+    ) {
+      return this->cd_sorting(fit, distance, order, size);
+    }
 
   };
 
